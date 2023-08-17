7fb16da5db455479bfa4c6a901ecd76211090e2c
yeyoung25 <126673060+yeyoung25@users.noreply.github.com>
2023-08-18 06:00:52 +0900

    Update EVERLAND.html 230818 01

--- js/global.js
+++ js/global.js
@@ -1,45 +1,97 @@
-$(document).ready(function(){
+$(document).ready(function () {
     headerEvent();
     headerAct();
     meuuButton();
-});
-
-function headerScroll(){
-    if($(window).scrollTop() == 0 && !$('#fnbPanel').hasClass("act")){
-        $('header').removeClass("mouseOver");
-        $('.go_top').addClass("scrollTop");
-    }else{
-        $('header').addClass("mouseOver");
-        $('.go_top').removeClass("scrollTop");
-    };
+    gotop();
+    sumDetail();
+    customSlider(".banslider",'fade',true,1000,7000,true,false,1,1,0,0,null);
+    customSlider(".carouselWay4",'horizontal',false,500,4000,false,false,7,5,600,10,null);
+    customSlider(".picSliderE",'fade',true,1000,6500,true,false,1,1,952,0,'.actTabPager');
+  });
+  
+function customSlider(sliderName,modeVal,slideAuto,slideSpeed,slidePause,slideInfiniteLoop,slideControls,slideMaxSlides,slideMinSlides,slideSlideWidth,slideSlideMargin,slidePagerCustom){
+    $(sliderName).bxSlider({
+        mode: modeVal,
+        auto: slideAuto,
+        speed: slideSpeed,
+        pause: slidePause,
+        infiniteLoop: slideInfiniteLoop, 
+        controls: slideControls,
+        maxSlides: slideMaxSlides,
+        minSlides: slideMinSlides,
+        slideWidth: slideSlideWidth,
+        slideMargin: slideSlideMargin,
+        pagerCustom: slidePagerCustom
+    });
 }
 
-function headerEvent(){
-    $('header').hover(function(){
-        $('header').addClass("mouseOver");
-    },function(){
-        if($(window).scrollTop() == 0 && !$('#fnbPanel').hasClass("act")){
-            $('header').removeClass("mouseOver");
+  function headerScroll() {
+    if ($(window).scrollTop() == 0 && !$("#fnbPanel").hasClass("act")) {
+      $("header").removeClass("mouseOver");
+      $(".go_top").addClass("scrollTop");
+    } else {
+      $("header").addClass("mouseOver");
+      $(".go_top").removeClass("scrollTop");
+    }
+  }
+  
+  function headerEvent() {
+    $("header").hover(
+      function () {
+        $("header").addClass("mouseOver");
+      },
+      function () {
+        if ($(window).scrollTop() == 0 && !$("#fnbPanel").hasClass("act")) {
+          $("header").removeClass("mouseOver");
         }
-    });
-
+      }
+    );
+  
     headerScroll();
     $(window).scroll(headerScroll);
-};
-
-function headerAct(){
+  }
+  
+  function headerAct() {
     let headerHight = $("header").innerHeight();
-    $('#fnbPanel').css("top", headerHight + "px");
-    $('.menuBar').click(function(){
-        $('#fnbPanel').toggleClass('act');
-        headerScroll();
-    })
-};
+    $("#fnbPanel").css("top", headerHight + "px");
+    $(".menuBar").click(function () {
+      $("#fnbPanel").toggleClass("act");
+      if (!$("#fnbPanel").hasClass("act")) {
+        $(".go_top").removeClass("scrollTop");
+      } else {
+        $(".go_top").addClass("scrollTop");
+      }
+    });
+  }
+  
+  function meuuButton() {
+    var menuBar = document.querySelector(".menuBar");
+    menuBar.addEventListener("click", () => {
+      menuBar.classList.toggle("toggle");
+    });
+  }
 
-function meuuButton(){
-    var menuBar = document.querySelector('.menuBar');
-    menuBar.addEventListener('click',()=>{
-        menuBar.classList.toggle('toggle')
-    })
-}
-
+  function gotop(){
+    $(".go_top").click(function () {
+        $('html, body').animate({scrollTop: '0'}, 680);
+    });
+  }
+//   function sumDetail(){ 
+//     $("summary").on("click",function(){
+//         $("details span").slideUp();
+//     });
+//     $("summary").on("click",function(){
+//         $("details span").slideDown();
+//     });
+//   }
+  function sumDetail(){ 
+    $("summary").click(function(){
+      if($("summary").hasClass("on")){
+        $(this).removeClass("on");
+        $("details span,details p").slideUp();
+      }else {
+        $(this).addClass("on");
+        $("details span, details p").slideDown();
+      }
+    });
+  }
--- js/page.js
+++ js/page.js
@@ -3,39 +3,20 @@
 // });
 
 
-//   function copy_to_map() {
-    
-//   }
-// const copy = (text) => {
-//   // 임시의 textarea 생성
-//   const $textarea = document.createElement("textarea");
-
-//   // body 요소에 존재해야 복사가 진행됨
-//   document.body.appendChild($textarea);
-  
-//   // 복사할 특정 텍스트를 임시의 textarea에 넣어주고 모두 셀렉션 상태
-//   $textarea.value = text;
-//   $textarea.select();
-  
-//   // 복사 후 textarea 지우기
-//   document.execCommand('copy');
-//   document.body.removeChild($textarea);
-// }
-
 const copy = (text) => {
+  alert("복사했습니다.");
   // 임시의 textarea 생성
   const $textarea = document.createElement("textarea");
 
   // body 요소에 존재해야 복사가 진행됨
   document.body.appendChild($textarea);
-  
-  // 복사할 특정 텍스트를 임시의 textarea에 넣어주고 모두 셀렉션 상태
-  $textarea.value = "199, EVERLAND-RO, POGOK-EUP, CHEOIN-GU, YONGIN-SI,GYEONGGI-DO, KOREA(Location number) 310 Jeondae-ri, Pogok-eup";
-  $textarea.select();
-  
-  // 복사 후 textarea 지우기
-  document.execCommand('copy');
-  document.body.removeChild($textarea);
-}
 
-copy("복사 테스트");
+  // 복사할 특정 텍스트를 임시의 textarea에 넣어주고 모두 셀렉션 상태
+  $textarea.value =
+    "199, EVERLAND-RO, POGOK-EUP, CHEOIN-GU, YONGIN-SI,GYEONGGI-DO, KOREA (Location number) 310 Jeondae-ri, Pogok-eup";
+  $textarea.select();
+
+  // 복사 후 textarea 지우기
+  document.execCommand("copy");
+  document.body.removeChild($textarea);
+};
