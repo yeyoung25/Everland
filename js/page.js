1d842cbe432b3539bc9a68969cbfa552e027c759
yeyoung25 <126673060+yeyoung25@users.noreply.github.com>
2023-08-21 18:19:37 +0900

    Update EVERLAND.html 230821 02

--- js/page.js
+++ js/page.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+
+});
+
 var copy = (text) => {
   alert("복사했습니다.");
   var $textarea = document.createElement("textarea");
@@ -5,4 +9,10 @@ var copy = (text) => {
     "199, EVERLAND-RO, POGOK-EUP, CHEOIN-GU, YONGIN-SI,GYEONGGI-DO, KOREA (Location number) 310 Jeondae-ri, Pogok-eup";
   $textarea.select();document.execCommand("copy");
   document.body.removeChild($textarea);
+};
+
+function manuNav(){
+  $('button').click(function(){
+  
+  });
 };
