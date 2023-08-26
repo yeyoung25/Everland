9ff0900a5752c2872fd45b86b2f7b23a214ab5a3
yeyoung25 <126673060+yeyoung25@users.noreply.github.com>
2023-08-27 03:53:06 +0900

    Update EVERLAND.html 230827 01

--- js/page.js
+++ js/page.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   justToggle($("div div:first-child nav div"));
+  trafTab();
 });
 
 
@@ -8,3 +9,15 @@ function justToggle(button){
     $(this).toggleClass("active");
   });
 }
+
+// function trafTab(){
+//   // .wrap ul li 를 클릭했을때 실행할 함수
+//   $('.trafContainer div:nth-child(2) ul li').click(function(){
+//   // .wrap ul li  active class 를 제거해주시고
+//   $('.trafContainer div:nth-child(2) ul li').removeClass('active');
+//   // 클릭이 이루어진 것에만 active class 를 추가해라.
+//   $(this).addClass('active');
+//   $('.trafContainer div:nth-child(2) ul li').removeClass('active');
+//   $('#' + $(this).attr('data-tab')).addClass('active');
+// });
+// }
