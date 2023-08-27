985e6fea184f71c0c3d531cd268d9d9501395547
yeyoung25 <126673060+yeyoung25@users.noreply.github.com>
2023-08-28 00:34:18 +0900

    Update EVERLAND.html 230828 01

--- js/page.js
+++ js/page.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
   justToggle($("div div:first-child nav div"));
+  howToggle($(".howContainer .detail_contents div .safetyBox div input"));
+  backBtn($(".backBtn"));
   trafTab();
 });
 
@@ -9,15 +11,26 @@ function justToggle(button){
     $(this).toggleClass("active");
   });
 }
+function howToggle(button){
+  $(button).click(function(){
+    $('#handicappedPopUp').addClass('active');
+    $('.closemodel').removeClass('active');
+  });
+}
+
+
 
 function trafTab(){
-  // ul li 를 클릭했을때 실행할 함수
   $('.trafContainer div:nth-child(2) ul li').click(function(){
-  // ul li  active class 를 제거해주시고
-  $('.trafContainer div:nth-child(2) ul li').removeClass('active');
-  // 클릭이 이루어진 것에만 active class 를 추가해라.
-  $(this).addClass('active');
-  $('.trafContainer div:nth-child(2) ul li').removeClass('active');
-  $('.' + $(this).attr('data-tab')).addClass('active');
-});
+    $('.trafContainer div:nth-child(2) ul li').removeClass('active');
+    $(this).addClass('active');
+    $('.trafContainer div:nth-child(2) div').removeClass('active');
+    $('.' + $(this).attr('data-tab')).addClass('active');
+  });
+}
+
+function backBtn(button){
+  $(button).click(function(){
+    history.go(-1);
+  });
 }
