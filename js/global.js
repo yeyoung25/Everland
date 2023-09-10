bbf14f7fd6e6954e68346626a2e74c33148eceb7
yeyoung25 <sevim0105@gmail.com>
2023-09-10 19:00:42 +0900

    Update EVERLAND.html 230910 02

--- js/global.js
+++ js/global.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
   customSlider(".carouselWay4",'horizontal',false,500,4000,false,false,5,1,325,35,null,'full',false,0,null);
   customSlider(".picSliderE",'fade',true,350,4500,true,false,1,1,0,0,'.actTabPager','full',false,0,null);
   customSlider(".picSliderH",'fade',true,350,4500,true,false,1,1,0,0,'.homTabPager','full',false,0,null);
-  customSlider(".speslider",'horizontal',false,0,0,false,true,5,5,214,45,null,'full',false,0,null);
+  customSlider(".speslider",'horizontal',false,500,0,false,true,5,5,214,45,null,'full',false,0,null);
   customSlider(".pagerSliderBox01 .fadeSlider",'fade',false,0,0,true,false,1,1,0,0,null,'full',false,0,".thumbPager01");
-  customSlider(".pagerSliderBox01 .thumbPager01",'horizontal',false,0,0,true,false,3,1,250,0,null,'full',false,1,null);
+  customSlider(".pagerSliderBox01 .thumbPager01",'horizontal',false,0,0,true,true,3,1,250,35,null,'full',false,1,null);
   customSlider(".pagerSliderBox02 .fadeSlider",'fade',false,0,0,true,false,1,1,0,0,null,'full',false,0,".thumbPager02");
-  customSlider(".pagerSliderBox02 .thumbPager02",'horizontal',false,0,0,true,false,3,1,250,0,null,'full',false,1,null);
+  customSlider(".pagerSliderBox02 .thumbPager02",'horizontal',false,0,0,true,true,3,1,250,35,null,'full',false,1,null);
   customSlider(".pagerSliderBox03 .fadeSlider",'fade',false,0,0,true,false,1,1,0,0,null,'full',false,0,".thumbPager03");
-  customSlider(".pagerSliderBox03 .thumbPager03",'horizontal',false,0,0,true,false,3,1,250,0,null,'full',false,1,null);
+  customSlider(".pagerSliderBox03 .thumbPager03",'horizontal',false,0,0,true,true,3,1,250,35,null,'full',false,1,null);
   headerAct();
   menuButton();
   gotop();
