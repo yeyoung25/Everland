2564481341e00bb46f8db7cc76e880e2f4d82353
yeyoung25 <sevim0105@gmail.com>
2023-11-26 01:57:15 +0900

    Update EVERLAND.html 231126 01

--- js/page.js
+++ js/page.js
@@ -156,6 +156,8 @@ function signUPcheck(){
 function smoothtoAnchor(){
   const urlParams = new URL(location.href).searchParams;
   const scroll = urlParams.get('scroll');
-
-  $('html,body').animate({ scrollTop: $("#" + scroll).offset()?.top - 150 }, 450, 'swing');
+  
+  if(scroll){
+    $('html,body').animate({ scrollTop: $("#" + scroll).offset()?.top - 150 }, 450, 'swing');
+  }
 }
