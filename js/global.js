476e814bcc0b7f515917ff1d24b5e44ab943e4ab
yeyoung25 <sevim0105@gmail.com>
2023-12-30 21:30:34 +0900

    Update EVERLAND.html 231230 02

--- js/global.js
+++ js/global.js
@@ -190,7 +190,7 @@ function mainScrollAct(){
   $('[data-scroll="transform"]').each(function () {
     if(
       $(window).scrollTop() >
-      $(this).offset().top - $(window).height() / 1.7
+      $(this).offset().top - $(window).height() / 1.1
     ) {
       $(this).addClass("active");
     } else {
@@ -201,7 +201,7 @@ function mainScrollAct(){
     $('[data-scroll="transform"]').each(function () {
       if(
         $(window).scrollTop() >
-        $(this).offset().top - $(window).height() / 1.7
+        $(this).offset().top - $(window).height() / 1.1
       ) {
         $(this).addClass("active");
       } else {
